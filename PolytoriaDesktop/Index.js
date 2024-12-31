4305c8f432de13c15264f08f34e056c0e174f51d
Redlava11wastaken <98986258+Redlava11wastaken@users.noreply.github.com>
2024-12-31 16:40:17 -0600

    added files
    ye

---
+++ PolytoriaDesktop/Index.js
@@ -0,0 +1,35 @@
+async function fetchData(){
+    try{
+
+        const PolyUser = document.getElementById("PolyName").value;
+        const proxyUrl = 'https://corsproxy.io/?key=ee73543a&url=';
+        const url = 'https://api.polytoria.com/v1/users/'+PolyUser;
+        const response = await fetch(proxyUrl + url);
+
+            if(!response.ok){
+                throw new Error("Could Not Fetch User")
+            }
+
+            const data = await response.json();
+            const PolyAvatar = data.thumbnail.avatar;
+            const user = data.username
+            const imgElement = document.getElementById("PolyAvatar")
+            const nameElement = document.getElementById("username")
+            const memebertext = document.getElementById("member")
+            const isplus = data.membershipType;
+            nameElement.innerHTML = user;
+            imgElement.src = PolyAvatar;
+            imgElement.style.display = "block";
+            if (isplus == "plusDeluxe"){
+                memebertext.innerHTML = "+";
+                memebertext.style.color = "blueviolet";
+            }
+            else if (isplus == "plus"){
+                memebertext.innerHTML = "+";
+                memebertext.style.color = "aqua";
+            }
+    }       
+    catch(error){
+        console.error(error);
+    }
+}
