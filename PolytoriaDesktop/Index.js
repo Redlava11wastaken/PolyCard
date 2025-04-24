46ccbab458d77a83203ff6f17f76fe3fa75d3f22
Redlava11wastaken <98986258+Redlava11wastaken@users.noreply.github.com>
2025-04-24 17:39:17 -0500

    Updated css
    made website css look better

--- PolytoriaDesktop/Index.js
+++ PolytoriaDesktop/Index.js
@@ -2,7 +2,7 @@ async function fetchData(){
     try{
 
         const PolyUser = document.getElementById("PolyName").value;
-        const proxyUrl = 'https://corsproxy.io/?key=______url='; // add corsproxy key
+        const proxyUrl = 'https://corsproxy.io/?key=ADDKEYHERE&url=';
         const url = 'https://api.polytoria.com/v1/users/'+PolyUser;
         const response = await fetch(proxyUrl + url);
 
@@ -20,14 +20,6 @@ async function fetchData(){
             nameElement.innerHTML = user;
             imgElement.src = PolyAvatar;
             imgElement.style.display = "block";
-            if (isplus == "plusDeluxe"){
-                memebertext.innerHTML = "+";
-                memebertext.style.color = "blueviolet";
-            }
-            else if (isplus == "plus"){
-                memebertext.innerHTML = "+";
-                memebertext.style.color = "aqua";
-            }
     }       
     catch(error){
         console.error(error);
